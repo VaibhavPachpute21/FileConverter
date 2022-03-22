a25bfdac3c0209d7730221d7c8a42f0d4df8c526
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-03-22 18:40:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  convertedJsonData!: String;
+
   title = 'tojson';
+  fileUpload(event: any) {
+    const selectedFile = event.target.files[0];
+     const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event: any) => {
+      let binaryData = event.target?.result;
+      let workbook = XLSX.read(binaryData, { type: 'binary' });
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        console.log(data);
+        this.convertedJsonData = JSON.stringify(data, undefined, 4);
+      })
+    }
+ 
+  }
 }
